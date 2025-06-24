5ac5566a3494cd3b218258a96e982451a97b80ec
louis melchiorre <ltm893@icloud.com>
2025-06-24 09:06:32 -0400

    s3 stuff

---
+++ create_s3.py
@@ -0,0 +1,51 @@
+
+import boto3
+from botocore.exceptions import ClientError
+
+import logging
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s - %(levelname)s - %(message)s')
+
+
+client = boto3.client('s3')
+bucket = 'ltm893-bag-writings'
+
+
+def check_create_bucket(bucket):
+    message = {}
+    try:
+        client.head_bucket(Bucket=bucket)
+        logging.info(f"{bucket} s3 bucket found")
+           
+       
+    except ClientError as e:
+        if e.response['Error']['Code'] == '404':
+            logging.info(f"Bucket {bucket} not found or you don't have access. Creattin {bucket}")
+
+            try: 
+                client.create_bucket(
+                Bucket=bucket,
+                CreateBucketConfiguration={'LocationConstraint': 'us-east-2' }
+                )
+                logging.info(f"{bucket} created")
+            except ClientError as e:
+                logging.exception("Exception occurred: %s", f"Bucket {bucket} not created" ,str(e))
+        else:
+            logging.exception("Exception occurred:", str(e))
+        
+def check_create_prefix(bucket,writers_dir):
+    try: 
+        client.head_object(Bucket=bucket,Key=writers_dir)
+        logging.info(f"{writers_dir} exists")
+    except ClientError as e:
+        if e.response['Error']['Code'] == '404':
+           logging.exception("Exception occurred: %s", f"Prefix {writers_dir} does not exist in {bucket} bucket" )
+           print("oops")
+
+
+               
+
+
+if __name__=="__main__":
+    check_create_bucket(bucket)
+    check_create_prefix(bucket,'Washington')
